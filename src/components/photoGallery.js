f7dede7b994c5cee027149afbfc1ac105559781e
Phillip Troutman <philliptroutman21@gmail.com>
2018-03-30 14:43:41 -0700

    addition of photo gallery and other dependesies to photogallery

--- src/components/photoGallery.js
+++ src/components/photoGallery.js
@@ -1,13 +1,24 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react';
+import Entry from './entry';
 
-class PhotoGallery extends Component{
+class PhotoGallery extends Component {
 
     render() {
-        
+        // temp. images to test with
+        let tempPics = [
+            'https://goo.gl/WBG2F4',
+            'https://goo.gl/34b24F',
+            'https://goo.gl/ay2Qtc',
+            'https://goo.gl/3uS5nn',
+            'https://goo.gl/qyCFzs'
+        ];
+        let images = tempPics.map((e, i) => <Entry src={e} key={i} />);
         return (
             <div id='photoGallery'>
-            'im the gallery'
+                {images}
             </div>
-        ); 
+        );
     }
 }
+
+export default PhotoGallery;
