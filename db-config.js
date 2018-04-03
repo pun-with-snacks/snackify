8080758409b276e6f21fa41a32c37934afe1e2e4
Riyad Shauk <riyadshauk@users.noreply.github.com>
2018-04-03 16:25:54 -0700

    Added psql command as a comment.

--- db-config.js
+++ db-config.js
@@ -1,3 +1,4 @@
+// for direct access in psql, run this command: `postgres://fkbjfolf:6wxiF5xuhWNZDFSU6sjcdK51U3voWfgz@baasu.db.elephantsql.com:5432/fkbjfolf`
 const user = 'fkbjfolf';
 const password = '6wxiF5xuhWNZDFSU6sjcdK51U3voWfgz';
 const host = 'baasu.db.elephantsql.com';
